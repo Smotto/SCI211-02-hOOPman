09b900131016c693ad168c1df5e5b38795e06e3d
Jarod Castillo <jcastillo1@mail.sfsu.edu>
2019-10-14 13:22:40 -0700

    Created .gitignore and files for the assignment.

---
+++ src/Human.java
@@ -0,0 +1,10 @@
+public class Human
+{
+    // Default constructor, don't touch!
+    public Human(){}
+    // The constructor that is used.
+    public Human(T arr, T torso, T legs)
+    {
+
+    }
+}
