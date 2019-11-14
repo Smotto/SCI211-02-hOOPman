e75634f817c1d10920db98104bc8d48171ef1710
Jarod Castillo <43973221+Smotto@users.noreply.github.com>
2019-11-14 11:27:25 -0800

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -3,7 +3,7 @@ public class Human
     // Default constructor, don't touch!
     public Human(){}
     // The constructor that is used.
-    public Human(T arr, T torso, T legs)
+    public Human(Head head, Torso torso, Legs legs)
     {
 
     }
